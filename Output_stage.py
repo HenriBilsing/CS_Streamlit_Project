a764315a66fb8c1f04ff686d8869a089f7028090
Lonzo14 <91062425+Lonzo14@users.noreply.github.com>
2023-12-01 09:51:33 +0100

    Update Output_stage.py

--- Output_stage.py
+++ Output_stage.py
@@ -2,6 +2,33 @@ import streamlit as st
 from streamlit_geolocation import streamlit_geolocation
 import requests
 import pandas as pd
+import math
+
+def haversine_distance(coord1, coord2):
+    # Radius of the Earth in kilometers
+    R = 6371.0
+
+    lat1, lon1 = coord1
+    lat2, lon2 = coord2
+
+    # Convert latitude and longitude from degrees to radians
+    lat1_rad = math.radians(lat1)
+    lon1_rad = math.radians(lon1)
+    lat2_rad = math.radians(lat2)
+    lon2_rad = math.radians(lon2)
+
+    # Difference in coordinates
+    dlat = lat2_rad - lat1_rad
+    dlon = lon2_rad - lon1_rad
+
+    # Haversine formula
+    a = math.sin(dlat / 2)**2 + math.cos(lat1_rad) * math.cos(lat2_rad) * math.sin(dlon / 2)**2
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+
+    distance = R * c
+
+    return distance
+
 
 # Function--------------------------------------------------------------------------
 
