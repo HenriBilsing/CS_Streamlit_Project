3c88aae0fd97facf47c3d2b21071d7d120815b31
Lonzo14 <91062425+Lonzo14@users.noreply.github.com>
2023-12-06 16:09:26 +0100

    Update Output_stage.py

--- Output_stage.py
+++ Output_stage.py
@@ -65,4 +65,36 @@ businesses = businesses_in_radius(user_coordinates, radius_km, business_type, da
 businesses
 
 
+# Streamlit app
+def main():
+    st.title('Business Locator')
+
+    # User inputs
+    lat = st.number_input('Latitude', value=40.7128)  # Default values as an example
+    lon = st.number_input('Longitude', value=-74.0060)
+    radius_km = st.number_input('Radius in km', value=5)
+    business_type = st.selectbox('Business Type', ['Restaurant', 'Cafe', 'Retail'])
+
+    # Button to perform action
+    if st.button('Find Businesses'):
+        user_coordinates = (lat, lon)
+        # Assuming 'data' is your dataset
+        results = businesses_in_radius(user_coordinates, radius_km, business_type, data)
+        
+        if results:
+            # Prepare DataFrame for st.map
+            df = pd.DataFrame({
+                'lat': [res['coordinates'][0] for res in results],
+                'lon': [res['coordinates'][1] for res in results]
+            })
+            st.map(df)
+            # Optionally display details in a table
+            st.write(results)
+        else:
+            st.write("No businesses found within the specified radius.")
+
+if __name__ == "__main__":
+    main()
+
+
 
