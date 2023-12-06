54bf76d27c5228b502c2c40e8dbd9f0d36388cc1
Lonzo14 <91062425+Lonzo14@users.noreply.github.com>
2023-12-06 14:20:52 +0100

    Update app-new map.py

--- app.py
+++ app.py
@@ -7,12 +7,12 @@ import math
 
 def main():
     st.title("Business Locator Application")
+    st.write("### Selected Location and Category")
 
     # Calling the input stage function
     user_location, business_category = Input_stage.input_stage()
 
     # Display the results from the input stage
-    st.write("### Selected Location and Category")
     st.write(f"Location: {user_location}")
     st.write(f"Category: {business_category}")
 
