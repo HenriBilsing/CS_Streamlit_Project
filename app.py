65bb176d9e802d49c2b03df5135a328517862391
Lonzo14 <91062425+Lonzo14@users.noreply.github.com>
2023-12-08 12:18:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -16,76 +16,55 @@ def main():
     st.write(f"Category: {business_category}")
     st.write(f"Radius: {radius_km}")
 
-    # Call the relevant API-related function (if any) from API_stage
-    api_data = API_stage.process_api_data({
-        'coordinates': {
-            'latitude': user_location[0], 
-            'longitude': user_location[1]
-        }, 
-        'category': business_category
-    })
+    # Button to perform action
+    if st.button('Find Businesses'):
+        # Call the relevant API-related function (if any) from API_stage
+        api_data = API_stage.process_api_data({
+            'coordinates': {
+                'latitude': user_location[0], 
+                'longitude': user_location[1]
+            }, 
+            'category': business_category
+        })
 
-    if api_data:
-        yelp_businesses = [
-            {
-                'name': business['name'],
-                'review': business.get('review', ''),
-                'type': business_category,
-                'latitude': business['coordinates']['latitude'],
-                'longitude': business['coordinates']['longitude'],
-            }
-            for business in api_data['results']  # Ensure this matches the structure of your API response
-        ]
+        if api_data:
+            # Get businesses of a specific type within the radius
+            results = Output_stage.process_output_data(api_data)
 
-        # Get businesses of a specific type within the radius
-        results = Output_stage.process_output_data(api_data)
-        
-        if results:
-            # Prepare DataFrame for pydeck
-            df = pd.DataFrame({
-                'lat': [res['latitude'] for res in results],
-                'lon': [res['longitude'] for res in results],
-                'name': [res['name'] for res in results],
-                'review': [res['review'] for res in results]
-            })
+            if results:
+                # Prepare DataFrame for pydeck
+                df = pd.DataFrame({
+                    'lat': [res['latitude'] for res in results],
+                    'lon': [res['longitude'] for res in results],
+                    'name': [res['name'] for res in results],
+                    'review': [res['review'] for res in results]
+                })
 
-            # Set the initial zoom level
-            initial_zoom = 11
-
-            # Adjust radius based on zoom level
-            radius = 10 * 2**(13 - initial_zoom)
-
-            # Create a pydeck Layer
-            layer = pdk.Layer(
-                'ScatterplotLayer',
-                df,
-                get_position=['lon', 'lat'],
-                get_color=[255, 30, 0, 160],
-                get_radius=radius,
-                pickable=True
-            )
-
-            # Set the viewport location
-            view_state = pdk.ViewState(
-                latitude=df['lat'].mean(),
-                longitude=df['lon'].mean(),
-                zoom=initial_zoom,
-                pitch=0
-            )
-
-            show_map_button = st.button('Show Map')
-        
-            if show_map_button:
-                # Render the map with pydeck
+                # Rendering the map with pydeck
+                initial_zoom = 11
+                radius = 10 * 2**(13 - initial_zoom)
+                layer = pdk.Layer(
+                    'ScatterplotLayer',
+                    df,
+                    get_position=['lon', 'lat'],
+                    get_color=[255, 30, 0, 160],
+                    get_radius=radius,
+                    pickable=True
+                )
+                view_state = pdk.ViewState(
+                    latitude=df['lat'].mean(),
+                    longitude=df['lon'].mean(),
+                    zoom=initial_zoom,
+                    pitch=0
+                )
                 r = pdk.Deck(
                     layers=[layer],
                     initial_view_state=view_state,
                     tooltip={"html": "<b>Name:</b> {name}<br><b>Review:</b> {review}"}
                 )
                 st.pydeck_chart(r)
-
-        else:
-            st.write("No businesses found within the specified radius.")
+            else:
+                st.write("No businesses found within the specified radius.")
 
 if __name__ == "__main__":
     main()
